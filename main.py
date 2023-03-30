1f638c877796e2b46ac2fa8121292f2e23070ab8
Leonel F. Benitez <leonelfbenitez@gmail.com>
2023-03-30 02:36:00 +0000

    more updates

--- main.py
+++ main.py
@@ -62,5 +62,5 @@ def index():
     return render_template("service_home.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+    app.run(host='0.0.0.0', port='3000', debug=True)
     
